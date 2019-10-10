5a16b74d73ea5380b1e2598ca3bc2e4d15a81e1f
Peter McDermott <mcdermpete@gmail.com>
2019-10-11 11:42:32 +1300

    Setup scene

---
+++ New Unity Project/Assets/Scripts/Cast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cast : MonoBehaviour
+{
+
+    public bool castAnimation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (castAnimation == true)
+        {
+            GetComponent<Animation>().Play("Me_Cast_Animation");
+        }
+        if (Input.GetButtonDown("Space"))
+        {
+            castAnimation = true;
+        }
+    }
+}
