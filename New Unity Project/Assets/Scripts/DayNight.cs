5dd1d75e787a1bf423fa73899ac64fcdf553182b
Peter McDermott <mcdermpete@gmail.com>
2019-10-13 19:26:48 +1300

    Sky and water

---
+++ New Unity Project/Assets/Scripts/DayNight.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class DayNight : MonoBehaviour
+{
+    public float rotateSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(Vector3.zero, Vector3.right, rotateSpeed * Time.deltaTime);
+        transform.LookAt(Vector3.zero);
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PostProcessEffects.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PostProcessEffects : MonoBehaviour
+{
+    public PostProcessVolume volume;
+
+    private ColorGrading colorGrade;
+    private Bloom bloom;
+    private float subtractPerSecond = 1f;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        volume.profile.TryGetSettings(out colorGrade);
+        volume.profile.TryGetSettings(out bloom);
+
+        colorGrade.tint.value = 0f;
+        bloom.intensity.value = 0f;
+        colorGrade.saturation.value = 0f;
+        colorGrade.ldrLutContribution.value = 0f;
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        colorGrade.tint.value = Mathf.Lerp(colorGrade.tint.value, 50, 0.5f * Time.deltaTime);
+        colorGrade.ldrLutContribution.value = Mathf.Lerp(colorGrade.ldrLutContribution.value, 1, 0.1f * Time.deltaTime);
+        colorGrade.saturation.value -= subtractPerSecond * Time.deltaTime;
+        bloom.intensity.value = Mathf.Lerp(bloom.intensity.value, 20, 0.5f * Time.deltaTime);
+    }
+}
