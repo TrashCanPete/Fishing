2b697aed62ddce5a3a6fe6a8a32b8ffef45d3fb9
Peter McDermott <mcdermpete@gmail.com>
2019-10-14 15:09:43 +1300

    Final build

--- New Unity Project/Assets/Scripts/PostProcessEffects.cs
+++ New Unity Project/Assets/Scripts/PostProcessEffects.cs
@@ -33,11 +33,17 @@ public class PostProcessEffects : MonoBehaviour
        // colorGrade.tint.value = Mathf.Lerp(colorGrade.tint.value, 50, 0.5f * Time.deltaTime);
        // colorGrade.ldrLutContribution.value = Mathf.Lerp(colorGrade.ldrLutContribution.value, 0.5f, 0.1f * Time.deltaTime);
         //colorGrade.saturation.value -= subtractPerSecond * Time.deltaTime;
-        bloom.intensity.value += 0.5f * Time.deltaTime;
-        if (bloom.intensity.value > 30)
+        bloom.intensity.value += 0.33f * Time.deltaTime;
+        if (bloom.intensity.value > 20)
         {
             bloom.intensity.value = 0;
         }
         colorGrade.ldrLutContribution.value = Mathf.PingPong(Time.time /60, 0.5f);
+       /* colorGrade.ldrLutContribution.value += 0.02f * Time.deltaTime;
+        if (colorGrade.ldrLutContribution.value > 0.9)
+        {
+            colorGrade.ldrLutContribution.value = 0;
+        }
+        */
     }
 }
